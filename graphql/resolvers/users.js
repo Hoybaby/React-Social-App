aa8ea66a8d57d21ada9070adacacf9443f7630e8
Hoybaby <mbartek436@gmail.com>
2021-08-03 10:10:14 -0400

    I am currentyl adding the bcrypt and the jsonwebtoken require to my users.js so it can be used. since bycrpyt is synchrnous it needs to be an await function

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,15 +1,26 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
+const bcrypt = require('bcryptjs.')
+
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     Mutation: {
         // inside of the register function, needs to take some input
         // parents give us resaults from the input from last step, in some cases, you can have mutiple resolvers
         // register(parent, args,context, info)
-        register(_, args,context, info) {
+        async register(
+            _,
+            { registerInput: { username, email, password, confirmPassword } },
+            context,
+            info
+        ) {
             // What i need to do: Validate user data,
             // make sure users doesn't already exist
             // hash password before storing it in the database and create an auth token
-            
-        }
-    }
-}
+            // the args
+            // ------------
+            // need to hash the password which is synchronous
+            password = await
+        },
+    },
+};
