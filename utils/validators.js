2f511842860b4d5cf50b87aad67bef00503090b9
Hoybaby <mbartek436@gmail.com>
2021-08-03 13:44:32 -0400

    finished writing some of therrors and validation

--- utils/validators.js
+++ utils/validators.js
@@ -10,6 +10,17 @@ module.exports.validateRegisterInput = (
     }
 
     if(email.trim() === '') {
-        errors.username = "Username must not be empty"
+        errors.email = "Email must not be empty"
+    } else {
+        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+        if(!email.match(regEx)) {
+            errors.email = "Email must be a valid email address"
+        }
+    }
+
+    if(password === '') {
+        errors.password = "Passwords must not be empty"
+    } else if(password = confirmPassword) {
+        errors.confirmPassword = 'Passwords must match'
     }
 }
