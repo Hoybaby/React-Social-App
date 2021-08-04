4e1f3969d79bf22b9b4e40ab03b50ad78c4ad78c
Hoybaby <mbartek436@gmail.com>
2021-08-04 12:35:25 -0400

    trying to see if these errors will register properly on error

--- utils/check-auth.js
+++ utils/check-auth.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server');
+
 const jwt = require('jsonwebtoken');
 // this secret will varify the token because we used it like this
 const {SECRET_KEY} = require('../config')
@@ -7,5 +9,17 @@ module.exports = (context) => {
     const authHeader = context.req.headers.authorization;
     if (authHeader) {
         // working with tokens, we send it with Bearer 'xxxx'
+        const token = authHeader.split('Bearer ')[1];
+        // if this fails, the token isnt valid
+        if(token) {
+            try {
+                const user = jwt.verify(token, SECRET_KEY);
+                return user
+            } catch(err) {
+                throw new AuthenticationError('Invalid/Expired token')
+            }
+        }
+        throw new Error('Authentication token must be \' Bearer [token'])
     }
+    throw new Error('Authentication header must be provided')
 }
