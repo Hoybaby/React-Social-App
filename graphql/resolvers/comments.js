899a64bab1a734f04a67f48f97675279dca5a983
Hoybaby <mbartek436@gmail.com>
2021-08-05 00:50:05 -0400

    making the new comments.js file with the schema Post, then importing the checkAuth for the username and error for graphql from other sources. when this is done. Making a new mutation that will handle some things such as createComment. In this create comment, I am checking if body is not empty as well as the post can be found/valid

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,7 +1,34 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check-auth');
+
+const {UserInputError} = require('apollo-server')
 
 module.exports - {
     Mutation: {
-        
+        createComment: async(_, {postId, body}, context) => {
+            // this will check if the user is logged in
+            const {username} = checkAuth(context)
+            if(body.trim() === '') {
+                throw new UserInputError('Empty Comment', {
+                    errors: {
+                        body: 'Comment body must not be empty'
+                    }
+                })
+            }
+
+            const post = await post.findById(postId);
+            if(post) {
+                // mongoose turns our data into a json models into json objects.
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOSString()
+                })
+                await post.save();
+                return post;
+            
+                // this is checking if post is not true, it will read this as typical logic suggests
+            } else throw new UserInputError('Post not found')
+        }
     }
 }
