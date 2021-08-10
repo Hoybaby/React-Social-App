9aee39156af6937f21d05ae1710b0335f089a07d
Hoybaby <mbartek436@gmail.com>
2021-08-10 16:47:52 -0400

    had to add a different dependency that i forgot which was apollo-http-link to create what i needed and changed the index.js so be an apolloProvider.. I AM RUNNING into an issue with npm start with my react which I will try to create a new branch to test the the solution it provides

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -2,3 +2,22 @@ import React from 'react';
 import App from './App';
 import ApolloClient from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory'
+import {createHttpLink} from 'apollo-link-http'
+import {ApolloProvider} from '@apollo/react-hooks';
+
+const httpLink = createHttpLink({
+    // this is just pointing towards our graphql server which is localhost 5000
+    uri: 'http://localhost:5000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+export default (
+    <ApolloProvider client={client}>
+        <App/>
+
+    </ApolloProvider>
+)
