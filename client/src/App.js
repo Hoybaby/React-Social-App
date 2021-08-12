4f7b031459488bb1ccb4147cbefd9f340bc96518
Hoybaby <mbartek436@gmail.com>
2021-08-12 09:48:29 -0400

    creating the code on the app.js that will use the react-router-dom to have paths go on my page exactly for the components I want to build later

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi WOrld!</h1>
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+
       
-    </div>
+    </Router>
   );
 }
 
