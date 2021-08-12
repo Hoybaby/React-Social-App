43d9795cad930eb18151811f7bdf04daa59ca840
Hoybaby <mbartek436@gmail.com>
2021-08-12 10:52:31 -0400

    There is two waysto add the container in semantic UI, it is to wrap it in the container i created from their source docs or make a seperate div with a className of ( ui container)

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,21 @@ import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register'
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
     <Router>
-      <MenuBar/>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
+      <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+      
 
+      
+      
       
     </Router>
   );
