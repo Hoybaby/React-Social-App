72466e89550e9100951f4084ff8cd94871a04afa
Hoybaby <mbartek436@gmail.com>
2021-08-12 13:42:24 -0400

    added two packages which are installed via npm install graphql graphql-tag, NExt have to make a variable to query up the infromation i have on my server.I use this variable in connection with useQuery

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag'
 
 function Home() {
+    // from useQuery we get, loading which is true and data. 
+    const {loading, data} = useQuery(FETCH_POSTS_QUERY)
+    if(data) {
+        console.log(data);
+    }
     return (
         <div>
             <h1>Home Page</h1>
@@ -9,5 +16,26 @@ function Home() {
     )
 }
 
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts{
+            id 
+            body 
+            createdAt 
+            username 
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments{
+                id 
+                username 
+                createdAt 
+                body
+            }
+        }
+    }
+`
 
 export default Home;
