d7eb8040178c44ad6a89a1596ddd498392ccbfa7
Hoybaby <mbartek436@gmail.com>
2021-08-12 10:56:41 -0400

    semantic ui has an integeration and has it behave as a different component. We want it to make a lin so we import link from react-router-dom. We then need an as={} and to={} to direct it

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Menu } from 'semantic-ui-react'
+import {Link } from 'react-router-dom';
 
 function MenuBar() {
 
@@ -15,24 +16,24 @@ function MenuBar() {
                         active={activeItem === 'home'}
                         // got rid of this.handleItemClick because it aint a class based component anymore
                         onClick={handleItemClick}
-                    />
-                    <Menu.Item
-                        name='home'
-                        // if activeItem is true, the messages will be highlighted
-                        active={activeItem === 'home'}
-                        // got rid of this.handleItemClick because it aint a class based component anymore
-                        onClick={handleItemClick}
+                        // semantic ui has an integeration and has it behave as a different component. We want it to make a link. We need an as={} and to={}
+                        as={Link}
+                        to={'/'}
                     />
                     <Menu.Menu position='right'>
                         <Menu.Item
                             name='login'
                             active={activeItem === 'login'}
                             onClick={handleItemClick}
+                            as={Link}
+                            to={'/login'}
                         />
                         <Menu.Item
                             name='register'
                             active={activeItem === 'register'}
                             onClick={handleItemClick}
+                            as={Link}
+                            to={'/register'}
                         />
                     </Menu.Menu>
                 </Menu>
