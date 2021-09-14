4e263c7b673fa6410d567c7d54082407a59c2c04
Hoybaby <mbartek436@gmail.com>
2021-09-14 14:39:54 -0400

    I thought i was running into an error but everything is good. Was getting an error with adding the post with an authentication error

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,16 +4,33 @@ import ApolloClient from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory'
 import {createHttpLink} from 'apollo-link-http'
 import {ApolloProvider} from '@apollo/react-hooks';
+import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     // this is just pointing towards our graphql server which is localhost 5000
     uri: 'http://localhost:5000'
 })
 
+
+const authLink = setContext(() => {
+    
+    const token = localStorage.getItem('jwtToken');
+
+    return{
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
+// with the authLink and concat, it should add the token to the page
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
+
+// this takesa  request but we dont need it this time around
+
 
 export default (
     <ApolloProvider client={client}>
