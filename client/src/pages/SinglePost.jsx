b67d46eaed0e28a48c069ef30229917eb03b1ae5
Hoybaby <mbartek436@gmail.com>
2021-09-17 15:17:27 -0400

    making up the whole dynamic of the page which will have two sections. I have to bring in information from the user which is being bought in from AuthContext and doing much more

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, {useContext } from 'react'
 import gql from 'graphql-tag'
+import {useQuery} from '@apollo/react-hooks'
+import { Card, Grid } from 'semantic-ui-react';
+import moment from 'moment'
+import { AuthContext } from '../context/auth';
+// import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function SinglePost(props) {
 
     const postId = props.match.params.postId;
+
+    const {user} = useContext(AuthContext)
+    // to see if we are getting it properly
+    console.log(postId);
+
+    const { data: {getPost}} = useQuery(FETCH_POSTS_QUERY, {
+        
+        variables: {
+            postId
+        }
+    });
+
+
+    let postMarkup;
+    // we might still be loading and it will get this, could be a spinner later
+    if(!getPost) {
+        postMarkup = <p>Loading post...</p>
+    } else {
+        const {id, body, createdAt, usename, comments, likes, likeCount, commentCount} = getPost;
+
+        postMarkup = (
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={2}>
+                        <Image
+                        src="src='https://react.semantic-ui.com/images/avatar/large/molly.png"
+                        size="small"
+                        float="right"/>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header>{username}</Card.Header>
+                                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                                <Card.Description>{body}</Card.Description>
+                            </Card.Content>
+                            <hr/>
+                            <Card.Content extra>
+                                <LikeButton user={user} post={{ id, likeCount, likes}}/>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    
+                </Grid.Row>
+            </Grid>
+        )
+    }
+
+    
 
 
 }
