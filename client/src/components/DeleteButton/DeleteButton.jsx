508856cc8a33d8bef984d213d0959fb7bdb91791
Hoybaby <mbartek436@gmail.com>
2021-09-18 14:35:12 -0400

    trying to add the logic the confirm modal from the semantic ui to confirm if the user actually wants to delete the post

--- client/src/components/DeleteButton/DeleteButton.jsx
+++ client/src/components/DeleteButton/DeleteButton.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import gql from 'graphql-tag'
 import {useMutation} from '@apollo/react-hooks';
 
-import {Button, Icon} from 'semantic-ui-react';
+import {Button, Confirm, Icon} from 'semantic-ui-react';
 
 function DeleteButton() {
 
+    const [confirmOpen, setConfirmOpen] = useState(false);
+
     const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+        update() {
+            // once we reach the update, we have confirmed the modal
+            setConfirmOpen(false);
+            // have to remove post from cache
+        }
         variables: {
             postId
         }
@@ -14,13 +21,22 @@ function DeleteButton() {
 
 
     return (
-        <Button as="div" 
-                    color="red" 
-                    onClick={() => console.log('Delete Post')}
-                    floated="right"
-                    >
-                        <Icon name="trash" style ={{margin: 0}}/>
-                    </Button>
+        <div>
+            <Button as="div" 
+            color="red" 
+            onClick={() => console.log('Delete Post')}
+            floated="right"
+            >
+                <Icon name="trash" style ={{margin: 0}}/>
+            </Button>
+            <Confirm
+                open={confirmOpen}
+                onCancel={()=> setConfirmOpen(false)}
+                onConfirm={deletePost}
+            />
+        </div>
+        
+
     )
     
 }
