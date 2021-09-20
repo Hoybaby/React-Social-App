31f5edb166c1bd02a7e9a54bef917821465e632c
Hoybaby <mbartek436@gmail.com>
2021-09-20 00:30:41 -0400

    fixed error on my single post with picture not loading

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -37,7 +37,7 @@ function SinglePost(props) {
                 <Grid.Row>
                     <Grid.Column width={2}>
                         <Image
-                        src="src='https://react.semantic-ui.com/images/avatar/large/molly.png"
+                        src='https://react.semantic-ui.com/images/avatar/large/molly.png'
                         size="small"
                         float="right"/>
                     </Grid.Column>
