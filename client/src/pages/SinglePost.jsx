c4e831339d53641b6a3803eb4c73dd66ce8813cf
Hoybaby <mbartek436@gmail.com>
2021-09-21 13:35:09 -0400

    added a input field with semantic ui css to make sure that the input field looks good and valid. had to make sure that the user is first log in and i did this with with user from AUthContext file.

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,7 +1,7 @@
-import React, {useContext } from 'react';
+import React, {useContext, useState } from 'react';
 import gql from 'graphql-tag'
-import {useQuery} from '@apollo/react-hooks'
-import { Button, Card, Grid, Image, Icon, Label } from 'semantic-ui-react';
+import {useQuery, useMutation} from '@apollo/react-hooks'
+import { Button, Card, Grid, Image, Icon, Label, Form } from 'semantic-ui-react';
 import moment from 'moment'
 import { AuthContext } from '../context/auth';
 // import { FETCH_POSTS_QUERY } from '../util/graphql';
@@ -17,12 +17,24 @@ function SinglePost(props) {
     // to see if we are getting it properly
     console.log(postId);
 
+    const [comment, setComment] = useState('');
+
     const { data: { getPost } = {}} = useQuery(FETCH_POST_QUERY, {
         
         variables: {
             postId
         }
     });
+
+    const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+        update() {
+            setComment()
+        },
+        variables: {
+            postId,
+            bodyId: comment
+        }
+    })
 
     function deletePostCallback() {
         props.history.push('/');
@@ -73,6 +85,28 @@ function SinglePost(props) {
                                 )}
                             </Card.Content>
                         </Card>
+                        {user && (
+                            <Card fluid>
+                                <Card.Content>
+                                <p>Post a Comment</p>
+                                    <Form> 
+                                        <div className="ui action input fluid">
+                                            <input type="text" 
+                                                placeholder="Comment" 
+                                                name='comment' 
+                                                value={comment} 
+                                                onChange={event => setComment(event.target.value)}
+                                            />
+                                        
+                                            <button type="submit" className="ui button teal" 
+                                                disabled={comment.trim() === ''} 
+                                                onClick={submitComment}> Submit
+                                            </button>
+                                        </div>
+                                    </Form>
+                                </Card.Content>
+                            </Card>
+                        )}
                         {comments.map(comment => (
                             <Card fluid key={comment.id}>
                                 <Card.Content>
@@ -95,6 +129,23 @@ function SinglePost(props) {
 
 
 }
+
+const SUBMIT_COMMENT_MUTATION = gql`
+
+    mutation($postId: ID!, $body: String!) {
+        createComment(postId: $postId, body: $body) {
+            id
+            comments{
+                id
+                body    
+                createdAt
+                username
+            }
+            commentCount
+        }
+    }
+
+`
 
 const FETCH_POST_QUERY = gql`
     query($postId: ID!) {
