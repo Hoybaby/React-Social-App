1d8ab4f75eaf4764e17467e36a8dc638a34cd95e
Hoybaby <mbartek436@gmail.com>
2021-09-21 13:12:26 -0400

    made the logic and the button show up on a comment page so now a user who made the comment can actually delete a comment that they liked

--- client/src/components/DeleteButton/DeleteButton.jsx
+++ client/src/components/DeleteButton/DeleteButton.jsx
@@ -5,25 +5,33 @@ import {useMutation} from '@apollo/react-hooks';
 import {Button, Confirm, Icon} from 'semantic-ui-react';
 import {FETCH_POSTS_QUERY} from '../../util/graphql';
 
-function DeleteButton({postId, callback}) {
+function DeleteButton({postId, commentId,  callback}) {
 
     const [confirmOpen, setConfirmOpen] = useState(false);
 
-    const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+    // if we want to delete a comment, if we have a commentId this means the button will know its on a comment instead of a post
+    const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+    const [deletePostOrMutation] = useMutation(mutation, {
         update(cache) {
             // once we reach the update, we have confirmed the modal
             setConfirmOpen(false)
             // have to remove post from cache
-            const data = cache.readQuery({
-                query: FETCH_POSTS_QUERY,
-            });
-            data.getPosts = data.getPosts.filter(p=> p.id !== postId);
-            cache.writeQuery({ query: FETCH_POSTS_QUERY, data})
+
+            // this if statement, if we are deleting a comment we dont want to do the below function, if we do delete a post, we will not have a comment ID and everything will run fine
+            if(!commentId) {
+                const data = cache.readQuery({
+                    query: FETCH_POSTS_QUERY,
+                });
+                data.getPosts = data.getPosts.filter(p=> p.id !== postId);
+                cache.writeQuery({ query: FETCH_POSTS_QUERY, data})
+            }
             // we might not always havea a callback so this if check makes sure everything works fine
             if(callback) callback();
         },
         variables: {
-            postId
+            postId,
+            commentId
         }
     });
 
@@ -40,7 +48,7 @@ function DeleteButton({postId, callback}) {
             <Confirm
                 open={confirmOpen}
                 onCancel={()=> setConfirmOpen(false)}
-                onConfirm={deletePost}
+                onConfirm={deletePostOrMutation}
             />
         </div>
         
@@ -53,5 +61,22 @@ const DELETE_POST_MUTATION = gql`
     mutation deletePost($postId: ID!) {
         deletePost(postId: $postId)
     }
+`
+
+const DELETE_COMMENT_MUTATION = gql`
+
+    mutation deleteComment($postId: ID!, $commentId: ID!) {
+        deleteComment(postId: $postId, commentId: $commentId) {
+            id
+            comments {
+                id
+                username
+                createdAt
+                body  
+            }
+            commentCount
+        }
+    }
+
 `
 export default DeleteButton;
